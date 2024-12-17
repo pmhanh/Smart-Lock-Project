1219a7c09df69e3558f3ce476102e2f3dc027519
nguyenduyan28 <nguyenduyan28@gmail.com>
2024-12-18 00:38:47 +0700

    update sign up with activation

--- node-web/controllers/firebase-auth-controller.js
+++ node-web/controllers/firebase-auth-controller.js
@@ -12,54 +12,63 @@ const {
 
  const auth = getAuth();
  const { v4: uuidv4 } = require('uuid');
- const {ref, set} = require('firebase/database')
+ const {ref, set, get, child,  update} = require('firebase/database')
  function generateUserId(){
   return uuidv4()
 
  }
 
 
+
+
+
  class FirebaseAuthController{
   registerUser(req, res) {
-    console.log(req.body);
     const email = req.body.email;
     const username = req.body.username;
     const password = req.body.password;
-
+  
     if (!email || !password || !username) {
-        return res.status(422).json({
-            email: "Email is required",
-            password: "Password is required",
-            username: "User name is required"
-        });
+      return res.status(422).json({
+        email: "Email is required",
+        password: "Password is required",
+        username: "User name is required"
+      });
     }
-
-    const auth = getAuth(); // Initialize Firebase Auth
-
+  
+    const auth = getAuth();
+  
     createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+      .then(async(userCredential) => {
         const user = userCredential.user;
-
+        const dbRef = ref(db);
+        const snapshot = await get(child(dbRef, `users/${user.uid}`));
+        if (!snapshot.exists()) {
+          // Store user data in Realtime Database
+          await set(ref(db, `users/${user.uid}`), {
+            username: username ,
+            email: email,
+            createdAt: new Date().toISOString(),
+            activate_email: false
+          });
+        } 
         // Send verification email
         return sendEmailVerification(user).then(() => {
-            // Store user data in Realtime Database
-            let userId = generateUserId()
-            return set(ref(db, 'users/' + userId),{
-                username,
-                email,
-                createdAt: new Date(),
-                emailVerified: false // Initially set to false
-            });
+          res.render("notify_password", {
+            message: "Check your email to activate your account."
+          });
         });
-    })
-      .then(() => {
-          res.render('notify_password')
       })
       .catch((error) => {
-          console.error(error);
-          res.status(500).json({ error: error.message || "Error when registering user" });
+        console.error(error);
+        res.status(500).json({ error: error.message || "Registration failed" });
       });
-}
+  }
+  
+  
+
+
+  
   loginUser(req, res){
     const email = req.body.email;
 
@@ -72,7 +81,21 @@ const {
     }
     const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+      .then(async(userCredential) => {
+        const user = userCredential.user;
+        if (!user.emailVerified) {
+          await signOut(auth)
+          return res.status(403).json({
+            error: "Please verify your email address before logging in."
+          });
+        }
+        // Check if user exists in Realtime Database
+        const updates = {
+          activate_email: true
+        }
+        const dbRef = ref(db, `users/${user.uid}`);
+        const snapshot = await get(child(dbRef, `users/${user.uid}`));
+        await update(dbRef, updates)
         const idToken = userCredential._tokenResponse.idToken
         if (idToken) {
             res.cookie('access_token', idToken, {
